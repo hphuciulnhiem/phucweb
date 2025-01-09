4bef78867686b2b6d19d1a34ceff28e65c491dc2
phan hong phuc <phucphan1512@gmail.com>
2025-01-09 23:30:43 +0700

    DSgfdg

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -48,6 +48,4 @@ class ProductsController < ApplicationController
         def product_params
           params.expect(product: [ :name, :description, :featured_image, :inventory_count ])
         end
-    
-    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,14 +13,13 @@ Rails.application.routes.draw do
 
   get "/products/new", to: "products#new"
   post "/products", to: "products#create"
-  
+
   get "/products/:id", to: "products#show"
-  
+
   get "/products/:id/edit", to: "products#edit"
   patch "/products/:id", to: "products#update"
   put "/products/:id", to: "products#update"
-  
+
   delete "/products/:id", to: "products#destroy"
   root "products#index"
-
 end
